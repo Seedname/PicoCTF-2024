f8ff1f27c15fd0d6f37976d9fde8c37ba7b3cf68
Julian Dominguez <45999377+Seedname@users.noreply.github.com>
2024-03-12 14:04:30 -0400

    solved endianness

---
+++ endianness/find_endianness.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <time.h>
+
+char *find_little_endian(const char *word)
+{
+    size_t word_len = strlen(word);
+    char *little_endian = (char *)malloc((2 * word_len + 1) * sizeof(char));
+
+    for (size_t i = word_len; i-- > 0;)
+    {
+        snprintf(&little_endian[(word_len - 1 - i) * 2], 3, "%02X", (unsigned char)word[i]);
+    }
+
+    little_endian[2 * word_len] = '\0';
+    return little_endian;
+}
+
+char *find_big_endian(const char *word)
+{
+    size_t length = strlen(word);
+    char *big_endian = (char *)malloc((2 * length + 1) * sizeof(char));
+
+    for (size_t i = 0; i < length; i++)
+    {
+        snprintf(&big_endian[i * 2], 3, "%02X", (unsigned char)word[i]);
+    }
+
+    big_endian[2 * length] = '\0';
+    return big_endian;
+}
+
+int main()
+{
+    char *challenge_word = "pzuyh";
+    char *little_endian = find_little_endian(challenge_word);
+    char *big_endian = find_big_endian(challenge_word);
+    // printf("%s", little_endian);
+    printf("%s", big_endian);
+
+    return 0;
+}
